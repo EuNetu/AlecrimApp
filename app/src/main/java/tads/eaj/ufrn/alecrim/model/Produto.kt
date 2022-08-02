fa4a50430a8453729e2abac1d01c9bcca0ed21ee
neto <euneto2501@gmail.com>
2022-08-02 17:46:44 -0300

    struct ready and working

---
+++ app/src/main/java/tads/eaj/ufrn/alecrim/model/Produto.kt
@@ -0,0 +1,9 @@
+package tads.eaj.ufrn.alecrim.model
+
+import java.math.BigDecimal
+
+data class Produto(
+        val nome: String,
+        val descricao: String,
+        val valor: BigDecimal
+)
